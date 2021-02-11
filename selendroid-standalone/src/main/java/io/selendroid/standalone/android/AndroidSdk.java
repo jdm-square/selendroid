bd794f7346de3d89afaca60ad9588e36e275c9d6
Josh Matthews <jmatthews@squareup.com>
2021-02-11 13:13:24 -0500

    Use latest SDK tools.

--- selendroid-standalone/src/main/java/io/selendroid/standalone/android/AndroidSdk.java
+++ selendroid-standalone/src/main/java/io/selendroid/standalone/android/AndroidSdk.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2012-2014 eBay Software Foundation and selendroid committers.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -66,7 +66,7 @@ public class AndroidSdk {
     StringBuffer command = new StringBuffer();
     command.append(toolsHome());
 
-    return new File(toolsHome(), "android" + platformExecutableSuffixBat());
+    return new File(toolsHome(), "avdmanager" + platformExecutableSuffixBat());
   }
 
   public static File emulator() {
@@ -77,7 +77,11 @@ public class AndroidSdk {
     StringBuffer command = new StringBuffer();
     command.append(androidHome());
     command.append(File.separator);
-    command.append("tools");
+    command.append("cmdline-tools");
+    command.append(File.separator);
+    command.append("latest");
+    command.append(File.separator);
+    command.append("bin");
     command.append(File.separator);
     return new File(command.toString());
   }
