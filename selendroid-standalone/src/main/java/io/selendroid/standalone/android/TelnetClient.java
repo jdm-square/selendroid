248f91d1c2b5f7feb132d5edd3d1aa9b8bb31b84
Josh Matthews <jmatthews@squareup.com>
2021-02-11 13:12:50 -0500

    Fix screenshots in inspector.

--- selendroid-standalone/src/main/java/io/selendroid/standalone/android/TelnetClient.java
+++ selendroid-standalone/src/main/java/io/selendroid/standalone/android/TelnetClient.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2014 eBay Software Foundation and selendroid committers.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -52,7 +52,7 @@ public class TelnetClient {
     try {
       sendQuietly(command);
       // read OK and ignore
-      in.readLine();
+      //log.info("XXXjdm: ignoring " + in.readLine());
       return in.readLine();
     } catch (Exception e) {
       String logMessage = String.format("Error reading response for command '%s'", command);
