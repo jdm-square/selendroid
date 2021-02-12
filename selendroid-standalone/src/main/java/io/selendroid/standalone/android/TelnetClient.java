518830c593a78ff2b40e101a5a05f84a73215853
Josh Matthews <jmatthews@squareup.com>
2021-02-12 18:10:04 -0500

    Authenticate when starting telnet client.

--- selendroid-standalone/src/main/java/io/selendroid/standalone/android/TelnetClient.java
+++ selendroid-standalone/src/main/java/io/selendroid/standalone/android/TelnetClient.java
@@ -16,11 +16,14 @@ package io.selendroid.standalone.android;
 import io.selendroid.standalone.exceptions.AndroidDeviceException;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.io.FileUtils;
 
 public class TelnetClient {
   Socket socket = null;
@@ -29,11 +32,13 @@ public class TelnetClient {
   private Logger log = Logger.getLogger(TelnetClient.class.getName());
 
   public TelnetClient(Integer port) throws AndroidDeviceException {
+    String authTokenPath = null;
     try {
       socket = new Socket("127.0.0.1", port);
       out = new PrintWriter(socket.getOutputStream(), true);
       in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+      boolean awaitingAuthTokenPath = false;
       while (true) {
         String line = in.readLine();
         if (line == null) {
@@ -41,10 +46,25 @@ public class TelnetClient {
         }
         if (line.equals("OK")) {
           break;
+        } else if (line.startsWith("Android Console: you can find your <auth_token> in")) {
+          awaitingAuthTokenPath = true;
+        } else if (awaitingAuthTokenPath) {
+          authTokenPath = line.substring(1, line.length() - 1);
+          awaitingAuthTokenPath = false;
         }
       }
     } catch (Exception e) {
       throw new AndroidDeviceException("Cannot establish a connection to device.", e);
+    }
+
+    if (authTokenPath != null) {
+      File tokenFile = new File(authTokenPath);
+      try {
+        String token = FileUtils.readFileToString(tokenFile);
+        this.sendQuietly("auth " + token);
+      } catch (IOException e) {
+        log.warning("Error reading auth token from " + tokenFile);
+      }
     }
   }
 
@@ -52,8 +72,16 @@ public class TelnetClient {
     try {
       sendQuietly(command);
       // read OK and ignore
-      //log.info("XXXjdm: ignoring " + in.readLine());
-      return in.readLine();
+      String result = in.readLine();
+      if (result.equals("OK")) {
+        result = in.readLine();
+      } else {
+        String ok = in.readLine();
+        if (!ok.equals("OK")) {
+          log.warning("Unexpected output instead of OK: " + ok);
+        }
+      }
+      return result;
     } catch (Exception e) {
       String logMessage = String.format("Error reading response for command '%s'", command);
       log.log(Level.WARNING, logMessage, e);
